70f3c4e17d71944d85f1428f19037287b6951651
Go Tajimi <tajimigo@gmail.com>
2021-12-30 10:59:43 +0000

    changes

--- dataset.py
+++ dataset.py
@@ -1,23 +1,73 @@
 from yahoo_finance_api2 import share
 from yahoo_finance_api2.exceptions import YahooFinanceError
+import pandas as pd
+import numpy as np
 
 
 class Dataset():
-		def __init__(
-				Code: str = None,
-				Date: str = "2018-01-01", 
-				End_Date: str = "2021-12-31", # for debug
-		) -> None:
-				single_data = share.Share("3092.T").get_historical( # zozo
-				    share.PERIOD_TYPE_YEAR,
-				    6,
-				    share.FREQUENCY_TYPE_DAY,
-				    1
-				)
-				# data preprocessing
-				# split to train / val / test
-				self.train_data = (train_X, train_Y, timestamp_train)
-				self.val_data = (val_X, val_Y, timestamp_val)
-				self.test_data = (test_X, test_Y, timestamp_test)
-				# ***_X,Y consists of 
-				# ([... Close value ...], [... Open value ...])
+	def __init__(
+			self,
+			Code: str = None,
+			Date: str = "2018-01-01", 
+			End_Date: str = "2021-12-31", # for debug
+	) -> None:
+		share_data = share.Share("3092.T").get_historical( # zozo
+		    share.PERIOD_TYPE_YEAR,
+		    6,
+		    share.FREQUENCY_TYPE_DAY,
+		    1
+		)
+		df = self._share_to_df(share_data)
+
+		#TODO: ad-hoc.
+		train_X, train_Y, val_X, val_Y, test_X, test_Y, test_open_Y, timestamp_train_Y, timestamp_val_Y, timestamp_test_Y = self._split_data(df, Date)
+
+		self.train_data = (train_X, train_Y, timestamp_train_Y)
+		self.val_data = (val_X, val_Y, timestamp_val_Y)
+		self.test_data = (test_X, test_Y, timestamp_test_Y)
+		self.test_open_data = (test_open_Y, timestamp_test_Y)
+
+	@staticmethod
+	def _split_data(df, Date):
+		df["Date"] = pd.to_datetime(df["Date"]) #TODO: this is just a WA
+		data_train_raw = df["Adj. Close"][df["Date"] < Date].to_numpy()
+		data_valtest_raw = df["Adj. Close"][df["Date"] >= Date].to_numpy()
+		data_valtest_open = df["open"][df["Date"] >= Date].to_numpy()
+		# import pdb;pdb.set_trace()
+		timestamp_train_Y = df["Date"][df["Date"] < Date]
+		timestamp_valtest_Y = df["Date"][df["Date"] >= Date]
+		timestamp_val_Y = timestamp_valtest_Y[:timestamp_valtest_Y.shape[0] // 2]
+		timestamp_test_Y = timestamp_valtest_Y[timestamp_valtest_Y.shape[0] // 2:]
+
+		def _data_reshape(data):
+			reshaped = []
+			for i in range(0, len(data) - 5):
+				reshaped.append(data[i : i + 5].copy())
+
+			reshaped_x = np.array(reshaped[0 : -1]).reshape((-1, 5, 1))
+			reshaped_y = np.array(reshaped[1:]).reshape((-1, 5, 1))
+			return reshaped_x, reshaped_y
+
+		train_X, train_Y = _data_reshape(data_train_raw)
+		valtest_X, valtest_Y = _data_reshape(data_valtest_raw)
+		_, valtest_open_Y = _data_reshape(data_valtest_open)
+
+		val_X = valtest_X[:valtest_X.shape[0] // 2]
+		val_Y = valtest_Y[:valtest_Y.shape[0] // 2]
+		test_X = valtest_X[valtest_X.shape[0] // 2:]
+		test_Y = valtest_Y[valtest_Y.shape[0] // 2:]
+		test_open_Y = valtest_open_Y[valtest_open_Y.shape[0] // 2:]
+
+		return train_X, train_Y, val_X, val_Y, test_X, test_Y, test_open_Y, timestamp_train_Y, timestamp_val_Y, timestamp_test_Y
+
+	@staticmethod
+	def _share_to_df(share_data):
+		columns = list(share_data.keys())
+		columns[0] = "Date"
+		columns[-2] = "Adj. Close"      # TODO: it's not actually Adjusted Close, but Close. 
+		df = pd.DataFrame(
+		    list(zip(*share_data.values())), 
+		    columns=columns
+		)
+		df["Date"] = pd.to_datetime(df["Date"], unit="ms").dt.date
+		return df
--- train.py
+++ train.py
@@ -1,5 +1,9 @@
 from models import Model
-
+import os
+from dataset import Dataset
+import hydra
+import tensorflow as tf
+import matplotlib.pyplot as plt
 
 def scheduler(epoch):
   
@@ -18,14 +22,57 @@ def scheduler(epoch):
   
   return lrate
 
+def plot(output_dir, hist):
+    history_dict = hist.history
 
-def main():
-	model = Model()
+    loss = history_dict["loss"]
+    root_mean_squared_error = history_dict["root_mean_squared_error"]
+    val_loss = history_dict["val_loss"]
+    val_root_mean_squared_error = history_dict["val_root_mean_squared_error"]
+
+    epochs = range(1, len(loss) + 1)
+
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    
+    fig.set_figheight(5)
+    fig.set_figwidth(15)
+    
+    ax1.plot(epochs, loss, label = "Training Loss")
+    ax1.plot(epochs, val_loss, label = "Validation Loss")
+    ax1.set(xlabel = "Epochs", ylabel = "Loss")
+    ax1.legend()
+    
+    ax2.plot(epochs, root_mean_squared_error, label = "Training Root Mean Squared Error")
+    ax2.plot(epochs, val_root_mean_squared_error, label = "Validation Root Mean Squared Error")
+    ax2.set(xlabel = "Epochs", ylabel = "Loss")
+    ax2.legend()
+    
+    plt.savefig(os.path.join(output_dir, "loss.png"))
+
+@hydra.main(config_name="config.yaml")
+def main(cfg):
+    if not os.path.exists(cfg.output_dir):
+        raise ValueError(f"""
+            Directory {cfg.output_dir} not found. Specify an appropriate output_dir.
+        """)
+    model = Model()
+    dataset = Dataset()
     callback = tf.keras.callbacks.LearningRateScheduler(scheduler)
-	model.compile(optimizer = tf.keras.optimizers.Adam(), loss = 'mse', metrics = tf.keras.metrics.RootMeanSquaredError())
-	hist = model.fit(AMZN_Train_X, AMZN_Train_Y, epochs = 100, validation_data = (AMZN_Val_X, AMZN_Val_Y), callbacks=[callback])
-	model.save("my_model")
-	plot(hist) # plot analysis
+    model.compile(
+        optimizer = tf.keras.optimizers.Adam(),
+        loss = 'mse',
+        metrics = tf.keras.metrics.RootMeanSquaredError()
+    )
+    hist = model.fit(
+        dataset.train_data[0],
+        dataset.train_data[1],
+        epochs = cfg.training.epochs,
+        validation_data = (dataset.val_data[0], dataset.val_data[1]),
+        callbacks=[callback]
+    )
+    model.save(os.path.join(cfg.output_dir, "my_model"))
+    plot(cfg.output_dir, hist) # plot analysis
 
-if "__name__" == "__main__":
+if __name__ == "__main__":
+    os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
     main()
