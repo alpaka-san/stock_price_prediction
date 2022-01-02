86ac4283592c8fe59a664929a1df054f41bacf3d
Go Tajimi <tajimigo@gmail.com>
2022-01-02 02:19:07 +0000

    add dataset_test

--- dataset_test.py
+++ dataset_test.py
@@ -1,14 +1,33 @@
 import unittest
 from dataset import Dataset
+import pandas as pd
 
 
 class TestDataset(unittest.TestCase):
 
     def test_instantiation(self):
         stock_code = "3092.T"
-        dataset = Dataset(stock_code)
+        date = "2019-01-01"
+        dataset = Dataset(stock_code, date)
+
         self.assertIsInstance(dataset, Dataset)
 
+        self.assertEqual(len(dataset.train_data), 3)
+        self.assertEqual(len(dataset.val_data), 3)
+        self.assertEqual(len(dataset.test_data), 3)
+        self.assertEqual(len(dataset.test_open_data), 2)
+
+    def test_instantiation_without_code(self):
+        with self.assertRaises(ValueError):
+            dataset = Dataset()
+
+    def test_get_historical_data_without_code(self):
+        with self.assertRaises(ValueError):
+            get_historical_data()
+
+    def test_get_historical_data(self):
+        data = get_historical_data("3092.T")
+        self.assertIsInstance(data, pd.Dataframe)
 
 if __name__ == "__main__":
     unittest.main()
